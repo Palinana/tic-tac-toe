25c7be8f52cdc3045268ac38559303ca7470d7fe
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2020-01-20 17:04:49 -0500

    Added functions to find a winner and check all boxes

---
+++ src/utils/functions.js
@@ -0,0 +1,44 @@
+export function findWinner(boxes) {
+    // Array with winning combinations
+    const rows = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for(let i=0; i<rows.length; i++){
+        const [a, b, c] = rows[i];
+
+        // Check if the game board contains winning combination
+        if (boxes[a] && boxes[a] === boxes[b] && boxes[a] === boxes[c]) {
+            // Return the winner ('x' or 'o')
+            return boxes[a];
+        }
+    }
+
+    return null;
+}
+
+export function areAllBoxesClicked(boxes) {
+    // the number of clicked boxes
+    let count = 0;
+
+    // Iterate over all boxes
+    boxes.forEach(item => {
+        if (item !== null) {
+            count++;
+        }
+    });
+
+    // Check if all boxes are clicked (filled)
+    if (count === 9) {
+        return true;
+    } else {
+        return false;
+    }
+}
