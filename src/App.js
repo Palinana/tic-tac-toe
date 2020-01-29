6abbf04202cc8d7961ff9efecfb6ad4832e904b2
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2020-01-28 21:33:35 -0500

    Removed score board

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Board from './components/Board/Board';
-import Scoreboard from './components/ScoreBoard/ScoreBoard';
 
 class App extends Component {
     render() {
         return (
-            // <div className="app">
-                <BrowserRouter>
-                    <Route exact path="/" component={Scoreboard}/>
-                    <Route path="/board" component={Board}/>
-                </BrowserRouter>
-            // </div>
+            <BrowserRouter>
+                <Route exact path="/" component={Board}/>
+            </BrowserRouter>
         )
     }
 }
